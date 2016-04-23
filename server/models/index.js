61c32b552b7545aabb1990a6ffa42844c0d7dba5
Stan Le <stan.le1107@gmail.com>
2016-04-22 19:06:09 -0700

    server

--- server/models/index.js
+++ server/models/index.js
@@ -4,7 +4,10 @@ var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
 var env = process.env.NODE_ENV || "development";
-var sequelize = new Sequelize("postgres://localhost:5432/edvancesdb");
+var sequelize = new Sequelize("postgres://localhost:5432/edvancesdb", {
+    username: 'postgres',
+    password: 'humantics'
+});
 var db = {};
 
 fs
