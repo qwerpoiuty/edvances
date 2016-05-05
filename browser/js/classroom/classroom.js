6ff676fea8c2b5c7b9c37ce349c2cd2d1c30e4d6
Stan Le <stan.le1107@gmail.com>
2016-05-05 12:35:52 -0700

    update

--- browser/js/classroom/classroom.js
+++ browser/js/classroom/classroom.js
@@ -18,8 +18,6 @@ app.controller('classroomCtrl', function($scope, AuthService, $state, user, jits
     $scope.title = "This is a filler title";
     $scope.teacher = "Mr. Le"
     $scope.date = Date.now()
-    // jitsi.init("test", document.getElementById('jitsiContainer'))
-    jitsi.start();
     $scope.hideFilm = function() {
         // console.log(jitsi.api)
     }
