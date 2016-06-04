d84451f4b71245e8979ecf84324d430e61aa057a
Stan Le <stan.le1107@gmail.com>
2016-06-03 23:01:27 -0700

    fix?

--- browser/js/overview/dashboard/dashboard.js
+++ browser/js/overview/dashboard/dashboard.js
@@ -13,7 +13,27 @@ app.config(function($stateProvider) {
     })
 })
 
-app.controller('dashboardCtrl', function($scope, $state, $rootScope, $timeout, user) {
+app.controller('dashboardCtrl', function($scope, $state, $rootScope, $timeout, user, $modal, $log) {
+    console.log(user)
+    $scope.browse = function() {
+        //this is going to be a student section for browsing classes
+        console.log('hello')
+    }
+
+    $scope.createClass = function() {
+        console.log('hello')
+        var modalInstance = $modal.open({
+            templateUrl: 'js/common/directives/modals/classCreation/create.html',
+            controller: 'createClassCtrl',
+            size: 'lg'
+        });
+
+        modalInstance.result.then(function(selectedItem) {
+            $scope.selected = selectedItem;
+        }, function() {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
+    }
 
     $scope.user = user
     $scope.date = new Date();
