06893f76ef5b0a4b8e9a1499844f091aec839c50
Stan Le <stan.le1107@gmail.com>
2016-07-08 17:11:01 -0700

    whiteboard fix

--- browser/js/classroom/classroom.js
+++ browser/js/classroom/classroom.js
@@ -18,27 +18,20 @@ app.config(function($stateProvider) {
     })
 });
 
-<<<<<<< HEAD
-app.controller('classroomCtrl', function($scope, AuthService, $state, user, socket) {
-    console.log('hello')
-=======
+
 app.controller('classroomCtrl', function($scope, AuthService, dataFactory, $state, user, socket, $window) {
->>>>>>> c691af172a79a765e61f2ed9a01881c9431dd352
-    $scope.user = user
-    $scope.date = Date.now()
-<<<<<<< HEAD
+
     $scope.live = false
 
-
+    //REMEMBER TO ADD THE MARGINS TO THE WHITEBOARD!!!!!
 
     socket.emit('join awesome room', location.pathname.slice(1));
-    socket.on('live class'function() {
+    socket.on('live class', function() {
         $scope.live = true
     })
-=======
     $state.transitionTo('info')
     socket.emit('join awesome room', location.pathname.slice(1));
->>>>>>> c691af172a79a765e61f2ed9a01881c9431dd352
+
 
     $scope.toggleClass = function() {
         socket.emit('toggle class')
