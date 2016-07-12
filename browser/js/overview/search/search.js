2bb6f424052cb32313d29d96a6c23303c28ed3cd
Stan Le <stan.le1107@gmail.com>
2016-07-12 14:19:43 -0700

    first commit

--- browser/js/overview/search/search.js
+++ browser/js/overview/search/search.js
@@ -8,7 +8,32 @@ app.config(function($stateProvider) {
 
 app.controller("searchCtrl", function($scope, dataFactory) {
 
-    $scope.search;
+    $scope.search = [{
+        title: "Math",
+        teacher: "Stan",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat, quisquam minus, illum omnis accusamus praesentium corporis sunt, laboriosam nostrum aliquid nulla, consectetur veniam amet quam laborum impedit dolorem fuga. Omnis.",
+        times: "Monday, 8:30, Wednesday 9:30"
+    }, {
+        title: "English",
+        teacher: "Stan",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat, quisquam minus, illum omnis accusamus praesentium corporis sunt, laboriosam nostrum aliquid nulla, consectetur veniam amet quam laborum impedit dolorem fuga. Omnis.",
+        times: "Monday, 8:30, Wednesday 9:30"
+    }, {
+        title: "History",
+        teacher: "Stan",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat, quisquam minus, illum omnis accusamus praesentium corporis sunt, laboriosam nostrum aliquid nulla, consectetur veniam amet quam laborum impedit dolorem fuga. Omnis.",
+        times: "Monday, 8:30, Wednesday 9:30"
+    }, {
+        title: "Science",
+        teacher: "Stan",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat, quisquam minus, illum omnis accusamus praesentium corporis sunt, laboriosam nostrum aliquid nulla, consectetur veniam amet quam laborum impedit dolorem fuga. Omnis.",
+        times: "Monday, 8:30, Wednesday 9:30"
+    }, {
+        title: "Programming",
+        teacher: "Stan",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat, quisquam minus, illum omnis accusamus praesentium corporis sunt, laboriosam nostrum aliquid nulla, consectetur veniam amet quam laborum impedit dolorem fuga. Omnis.",
+        times: "Monday, 8:30, Wednesday 9:30"
+    }];
 
 
 })
